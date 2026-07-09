d37b0d540f0abfd51b3315217f24534e9ea033a1
Mark Dumay <61946753+markdumay@users.noreply.github.com>
2026-07-09 19:59:45 +0200

    chore: keep committed dist/ unchanged (katex bump is out of scope)

--- dist/contrib/mathtex-script-type.js
+++ dist/contrib/mathtex-script-type.js
@@ -86,18 +86,20 @@ scripts.forEach(function (script) {
   if (!script.type || !script.type.match(/math\/tex/i)) {
     return -1;
   }
+
   const display = script.type.match(/mode\s*=\s*display(;|\s|\n|$)/) != null;
   const katexElement = document.createElement(display ? "div" : "span");
   katexElement.setAttribute("class", display ? "equation" : "inline-equation");
+
   try {
     katex__WEBPACK_IMPORTED_MODULE_0___default().render(script.text, katexElement, {
       displayMode: display
     });
   } catch (err) {
-    // eslint-disable-next-line no-console
-    console.error(err);
+    //console.error(err); linter doesn't like this
     katexElement.textContent = script.text;
   }
+
   script.parentNode.replaceChild(katexElement, script);
 });
 __webpack_exports__ = __webpack_exports__["default"];
